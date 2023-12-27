b8b79456a5f2e501543c69a95fe03b32d9db78b4
lucas <153396560+lucasknowsyou@users.noreply.github.com>
2023-12-27 15:26:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,59 @@
-(()=> {
-    let __webpack_modules__ = {
-        'yeah' : (module, exports, require) => {
-            module.exports = () => {
-                console.log('hi')
-            }
-        }
-    }
-    let __webpack_cache__ = {}
-    function require(moduleID) {
-        if (typeof __webpack_cache__[moduleID] !== 'undefined')  {
-            return __webpack_cache__[moduleID]
-        }
-        var module = __webpack_cache__[moduleID] = require.moduleFactory({
-            id : moduleID,
-            loaded : true,
-            exports: {}
-        })
-        __webpack_modules__[moduleID](module, module.exports, require)
-        return module
-    }
-    require.modules = __webpack_modules__
-    require.cache = __webpack_cache__
-    require.moduleFactory = (obj) => {
-        Object.defineProperty(obj, Symbol.toStringTag, {
-            value: "Module"
-        })
-        Object.defineProperty(obj, "__esModule", {
-            value: true
-        })
-        return obj
+(() => {
+    let config = {
+        wpName : 'webpackJsonp',
+        splitter : false
     }
 
-    require('yeah').exports()
-})()
+    let __webpack_modules__ = {};
+    let __webpack_cache__ = {};
+    function require(moduleID) {
+        if (typeof __webpack_cache__[moduleID] !== 'undefined') {
+            return __webpack_cache__[moduleID];
+        }
+        var module = (__webpack_cache__[moduleID] = require.moduleFactory({
+            id: moduleID,
+            loaded: false,
+            exports: {},
+        }));
+        __webpack_modules__[moduleID](module, module.exports, require);
+        module.loaded = true;
+        return module;
+    }
+
+    require.modules = __webpack_modules__;
+    require.cache = __webpack_cache__;
+    require.moduleFactory = (obj) => {
+        Object.defineProperty(obj, Symbol.toStringTag, {
+            value: 'Module',
+        });
+        Object.defineProperty(obj, '__esModule', {
+            value: true,
+        });
+        return obj;
+    };
+
+    if (config.splitter === true) {
+        self[config.wpName] = self[config.wpName] || [];
+        let originalPush = self[config.wpName].push;
+        self[config.wpName].push = function (entry) {
+            let chunkIds = entry[0];
+            let modules = entry[1];
+            if (entry[2]) {
+                entry[2](require)
+            }
+
+            let existingEntry = self[config.wpName].find(([existingChunkIds]) => {
+                return existingChunkIds.every((chunkId) => chunkIds.includes(chunkId));
+            });
+
+            if (existingEntry) {
+                let [existingChunkIds, existingModules] = existingEntry;
+                existingModules = Object.assign(existingModules, modules);
+                Object.assign(__webpack_modules__, modules);
+            } else {
+                originalPush.call(self[config.wpName], entry);
+                Object.assign(__webpack_modules__, modules);
+            }
+        };
+    }
+})();
